934f736c692a4da896a27ed2bc466a3fb1c05759
JalsanSarylov <67546894+JalsanSarylov@users.noreply.github.com>
2023-01-04 22:12:50 +0300

    add geocoder

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,7 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import React from "react";
+import "leaflet/dist/leaflet.css";
+import {LeafletControlGeocoder} from "./LeafletControlGeocoder" 
 
 export default function Map() {
   return (
@@ -8,6 +10,7 @@ export default function Map() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <LeafletControlGeocoder/>
     </MapContainer>
   );
 }
